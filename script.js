e1a40570780d2b642ddb865bb75e35a955259342
santoshmode07 <santoshmode7@gmail.com>
2025-10-17 21:14:43 +0530

    Added initial E-commerce website structure with product categories and dynamic loading

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('logoutBtn').addEventListener('click', function () {
+    const toast = new bootstrap.Toast(document.getElementById('logoutToast'));
+    toast.show();
+  });
